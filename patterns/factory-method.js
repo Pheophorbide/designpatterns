429bf83eb5170005b810de0765a4ed5ff8df8c38
pheophorbide <daria.paramonova@gmail.com>
2020-12-09 15:28:08 +0300

    added factory method

--- patterns/factory-method.js
+++ patterns/factory-method.js
@@ -3,3 +3,34 @@
 // или когда дынные беруться из разных источников доя одного объекта. Фабрика состоит из нескольких конструкторов -
 // конструктора, который создает объект
 // и класса, который вызывает это конструктор с определенными параметрами
+
+class Product {
+    constructor(type, price, minCount) {
+        this.type = type;
+        this.price = price;
+        this.minCount = minCount;
+    }
+
+    print() {
+        return `Продукт ${this.type} по цене ${this.price} p. доступен при минимальном заказе от ${this.minCount} шт.`;
+    }
+}
+
+class ProductFactory {
+    create(type) {
+        if(type === 'молоко') {
+            return new Product(type, 80, 10).print();
+        } else if (type === 'сыр') {
+            return new Product(type, 200, 15).print();
+        }
+
+        return `У нас нет информации о продукте ${type}`;
+    }
+}
+
+const milk = new ProductFactory().create('молоко');
+const cheese = new ProductFactory().create('сыр');
+const other = new ProductFactory().create('курочка');
+
+console.log(milk, cheese, other)
+
