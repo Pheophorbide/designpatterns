a9bea925a974ffab71f92ef9a424665749e0fa87
pheophorbide <daria.paramonova@gmail.com>
2020-12-11 17:35:17 +0300

    added prototype

---
+++ patterns/prototype.js
@@ -0,0 +1,21 @@
+// Порождаюший. Прототип. Создает клон объекта, который потом можно точечно менять
+
+class Table {
+    constructor(row, column, width) {
+        this.row = row;
+        this.column = column;
+        this.width = width;
+    }
+
+    produce() {
+        return new Table(this.row,this.column, this.width)
+    }
+}
+
+const prototypeTable = new Table(5,3, 500);
+const table1 = prototypeTable.produce();
+const table2 = prototypeTable.produce();
+table2.column = 2;
+table2.width = 400;
+
+console.log(table1, '<<>>', table2);
